4a37de0a186cf5206816ce46a699a6703b410f01
OchoittaDev <87886442+OchoittaDev@users.noreply.github.com>
2023-03-01 10:54:39 -0400

    Contribución de Alfonso Castellano

---
+++ routes/espacio.js
@@ -0,0 +1,28 @@
+var express = require ('express');
+var router = express.Router();
+var EspacioController = require('../Controllers/espacio.c');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.send(EspacioController.Obtener());
+  });
+  
+  router.get("/:id", function(req, res, next) {
+    res.send(EspacioController.ObtenerID(req.params.id));
+  });
+  
+  router.post('/', function(req,res,next){
+    const espacios = EspacioController.Agregar(req.body);
+    res.send(espacios);
+  })
+  
+  router.put('/:id', function(req,res,next){
+      res.send(EspacioController.ActualizadID(req.params.id, req.body)) //NO MODIFICAR
+  })
+  
+  router.delete("/:id", function(req, res, next) {
+    res.send(EspacioController.EliminarID(req.params.id));
+  });
+  
+
+module.exports = router
