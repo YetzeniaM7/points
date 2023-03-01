390f01e7172b6631e051521ae526fcfe4b1a3848
Aljofranc <126708926+Aljofranc@users.noreply.github.com>
2023-03-01 14:13:47 -0400

    Update espacio.js

--- routes/espacio.js
+++ routes/espacio.js
@@ -2,24 +2,24 @@ var express = require ('express');
 var router = express.Router();
 var EspacioController = require('../controllers/espacio.c');
 
-/* GET home page. */
+//Obtener espacios /espacio
 router.get('/', function(req, res, next) {
     res.send(EspacioController.Obtener());
   });
-  
+  //Obtener espacios por su id (/espacio/:id)
   router.get("/:id", function(req, res, next) {
     res.send(EspacioController.ObtenerID(req.params.id));
   });
-  
+  //Agregar espacios (Metodo post)
   router.post('/', function(req,res,next){
     const espacios = EspacioController.Agregar(req.body);
     res.send(espacios);
   })
-  
+  //Editar espacios mediante su ID /espacio/:id (Metodo patch)
   router.put('/:id', function(req,res,next){
       res.send(EspacioController.ActualizadID(req.params.id, req.body)) //NO MODIFICAR
   })
-  
+  //Eliminar espacios mediante su id /espacio/:id (Metodo delete)
   router.delete("/:id", function(req, res, next) {
     res.send(EspacioController.EliminarID(req.params.id));
   });
